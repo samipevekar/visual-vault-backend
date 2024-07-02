d8e1a5d61cc4d741c75d9b7fbe946ad26c9d67be
samipevekar <samipevekar499@gmail.com>
2024-07-02 16:10:52 +0530

    code optimized

--- db/db.js
+++ db/db.js
@@ -1,11 +1,15 @@
-const mongoose = require("mongoose")
-// mongodb+srv://samipevekar:visualvault7901@cluster0.ejkvgmn.mongodb.net - atlas url
-// mongodb://127.0.0.1:27017  - localhost url
-const connectMongo=()=>{
-    mongoose.connect(`mongodb+srv://samipevekar:visualvault7901@cluster0.ejkvgmn.mongodb.net/Practice`)
-    .then(()=>console.log("connected"))    
-    .catch((err)=>console.log(err))
+const mongoose = require("mongoose");
 
+const connectMongo = () => {
+    const options = {
+        maxPoolSize: 10, // Maintain up to 10 socket connections
+        serverSelectionTimeoutMS: 45000, // Keep trying to send operations for 5 seconds
+        socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+    };
+
+    mongoose.connect('mongodb+srv://samipevekar:visualvault7901@cluster0.ejkvgmn.mongodb.net/Practice', options)
+        .then(() => console.log("Connected to MongoDB Atlas"))
+        .catch((err) => console.log("Error connecting to MongoDB Atlas:", err));
 }
 
-module.exports = connectMongo
+module.exports = connectMongo;
